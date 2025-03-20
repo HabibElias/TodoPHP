749ee985ea1af147d6d9afa51bfb35aef1366da5
Habib Elias <habibelias234@gmail.com>
2025-03-20 12:44:10 -0900

    Revert "first version"
    This reverts commit 20d114ccf143fe40aa225f7ebd15677299bd8e51.

--- frontend/test/src/models/User.ts
+++
@@ -1,6 +0,0 @@
-export interface User {
-  id: number;
-  age: number;
-  name: string;
-  email: string;
-}
--- frontend/test/src/services/Apiclient.ts
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export const axiosInstance = axios.create({
-  url: "http://localhost/lab1/backend/routes",
-});
-
